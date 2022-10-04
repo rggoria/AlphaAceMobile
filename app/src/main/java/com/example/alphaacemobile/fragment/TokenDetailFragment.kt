99b96841938d1284857c42f8b57bed34bf35869a
Ram Emerson Goria <ramemersongoria.13.versus@gmail.com>
2022-10-04 23:24:46 +0800

    Semi-working Token Overview Functionality

--- app/src/main/java/com/example/alphaacemobile/fragment/TokenDetailFragment.kt
+++ app/src/main/java/com/example/alphaacemobile/fragment/TokenDetailFragment.kt
@@ -1,12 +1,25 @@
 package com.example.alphaacemobile.fragment
 
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.android.volley.Response
+import com.android.volley.RetryPolicy
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.alphaacemobile.Constant
 import com.example.alphaacemobile.R
 import com.example.alphaacemobile.databinding.FragmentTokenDetailBinding
+import com.example.alphaacemobile.model.HomeTopModel
+import org.json.JSONObject
 
 class TokenDetailFragment : Fragment() {
 
@@ -26,8 +39,204 @@ class TokenDetailFragment : Fragment() {
             val tvName = tvTokenDetailHeader
 
             tvName.text = "$tokenName"
-
+            getData(tokenName)
             return root
         }
     }
+
+    // Token Data
+    private fun getData(tokenName: String?) {
+        val url = Constant.BASE_URL + "get_token_detail"
+        val requestQueue = Volley.newRequestQueue(requireActivity())
+
+        val stringRequest = object : StringRequest(
+            Method.POST,url,
+            Response.Listener
+            { response ->
+                // Debugging purposes
+                Log.d("Token_Detail_Fragment", response)
+                val jsonObject = JSONObject(response)
+
+                if(jsonObject.get("response").equals("Successful")) {
+                    val jsonArray = jsonObject.getJSONArray("data")
+
+                    for (i in 0..jsonArray.length()-1) {
+                        val jo = jsonArray.getJSONObject(i)
+                        //store your variable
+                        val top = jo.getString("top").toString()
+                        val token_name = jo.getString("token_name").toString()
+                        val curr_prc = jo.getString("curr_prc").toString()
+                        val cir_supp = jo.getString("cir_supp").toString()
+                        val total_vol = jo.getString("total_vol").toString()
+                        val price_24h = jo.getString("price_24h").toDouble()
+                        val symbol = jo.getString("symbol").toString().uppercase()
+                        val reddit_link = jo.getString("reddit_link").toString()
+                        val facebook_link = jo.getString("facebook_link").toString()
+                        val linkedin = jo.getString("linkedin").toString()
+                        val discord_invite = jo.getString("discord_invite").toString()
+                        val twitter = jo.getString("twitter").toString()
+                        val telegram = jo.getString("telegram").toString()
+                        val coin_link = jo.getString("coin_link").toString()
+                        val website = jo.getString("website").toString()
+
+                        Log.d("top", top)
+                        Log.d("tk_name", token_name)
+                        Log.d("tk_price", curr_prc)
+                        Log.d("cir_supp", cir_supp)
+                        Log.d("total_vol", total_vol)
+                        Log.d("price_24h", price_24h.toString())
+                        Log.d("reddit_link", reddit_link)
+                        Log.d("facebook_link", facebook_link)
+                        Log.d("linkedin", linkedin)
+                        Log.d("discord_invite", discord_invite)
+                        Log.d("twitter", twitter)
+                        Log.d("telegram", telegram)
+                        Log.d("coin_link", coin_link)
+                        Log.d("website", website)
+
+
+                        // Price
+                        val vSymbol = binding.tvTokenDetailSymbol
+                        val vPrice = binding.tvTokenDetailPriceValue
+                        val vCirculatingSup = binding.tvTokenDetailCirculatingSupValue
+                        val vTotalVol = binding.tvTokenDetailVolumeValue
+                        val vMarketCap24 = binding.tvTokenDetailMarketCap24Value
+
+                        // Setting up data
+                        vSymbol.text = "$symbol"
+                        vPrice.text = "$curr_prc"
+                        vCirculatingSup.text = "$cir_supp"
+                        vTotalVol.text = "$total_vol"
+                        vMarketCap24.text = "$price_24h"
+                        //Market Price Coloring Coloring
+                        if (price_24h > 0) {
+                            vMarketCap24.setTextColor(Color.parseColor("#00FF00"))
+                        } else {
+                            vMarketCap24.setTextColor(Color.parseColor("#FF0000"))
+                        }
+
+                        // Showing and Hiding Icon Links
+                        if (reddit_link == "null"){ // Reddit
+                            binding.imTokenDetailSocialReddit.visibility =View.GONE
+                        } else {
+                            binding.imTokenDetailSocialReddit.visibility =View.VISIBLE
+                            binding.imTokenDetailSocialReddit.setOnClickListener {
+                                Toast.makeText(requireActivity(),"Reddit",Toast.LENGTH_SHORT).show()
+                                val urlIntent = Intent(Intent.ACTION_VIEW, Uri.parse(reddit_link))
+                                startActivity(urlIntent)
+                            }
+                        }
+                        if (facebook_link == "null"){ // Facebook
+                            binding.imTokenDetailSocialFacebook.visibility =View.GONE
+                        } else {
+                            binding.imTokenDetailSocialFacebook.visibility =View.VISIBLE
+                            binding.imTokenDetailSocialFacebook.setOnClickListener {
+                                Toast.makeText(requireActivity(),"Facebook",Toast.LENGTH_SHORT).show()
+                                val urlIntent = Intent(Intent.ACTION_VIEW, Uri.parse(facebook_link))
+                                startActivity(urlIntent)
+                            }
+                        }
+                        if (linkedin == "null"){ // Linkedin
+                            binding.imTokenDetailSocialLinkedin.visibility =View.GONE
+                        } else {
+                            binding.imTokenDetailSocialLinkedin.visibility =View.VISIBLE
+                            binding.imTokenDetailSocialLinkedin.setOnClickListener {
+                                Toast.makeText(requireActivity(),"Linkedin",Toast.LENGTH_SHORT).show()
+                                val urlIntent = Intent(Intent.ACTION_VIEW, Uri.parse(linkedin))
+                                startActivity(urlIntent)
+                            }
+                        }
+                        if (discord_invite == "null"){ // Discord
+                            binding.imTokenDetailSocialDiscord.visibility =View.GONE
+                        } else {
+                            binding.imTokenDetailSocialDiscord.visibility =View.VISIBLE
+                            binding.imTokenDetailSocialDiscord.setOnClickListener {
+                                Toast.makeText(requireActivity(),"Discord",Toast.LENGTH_SHORT).show()
+                                val urlIntent = Intent(Intent.ACTION_VIEW, Uri.parse(discord_invite))
+                                startActivity(urlIntent)
+                            }
+                        }
+                        if (twitter == "null"){ // Twitter
+                            binding.imTokenDetailSocialTwitter.visibility =View.GONE
+                        } else {
+                            binding.imTokenDetailSocialTwitter.visibility =View.VISIBLE
+                            binding.imTokenDetailSocialTwitter.setOnClickListener {
+                                Toast.makeText(requireActivity(),"Twitter",Toast.LENGTH_SHORT).show()
+//                                val urlIntent = Intent(Intent.ACTION_VIEW, Uri.parse(twitter))
+//                                startActivity(urlIntent)
+                            }
+                        }
+                        if (telegram == "null"){ // Telegram
+                            binding.imTokenDetailSocialTelegram.visibility =View.GONE
+                        } else {
+                            binding.imTokenDetailSocialTelegram.visibility =View.VISIBLE
+                            binding.imTokenDetailSocialTelegram.setOnClickListener {
+                                Toast.makeText(requireActivity(),"Telegram",Toast.LENGTH_SHORT).show()
+                                val urlIntent = Intent(Intent.ACTION_VIEW, Uri.parse(telegram))
+                                startActivity(urlIntent)
+                            }
+                        }
+                        if (coin_link == "null"){ // Coingecko
+                            binding.imTokenDetailSocialCoingecko.visibility =View.GONE
+                        } else {
+                            binding.imTokenDetailSocialCoingecko.visibility =View.VISIBLE
+                            binding.imTokenDetailSocialCoingecko.setOnClickListener {
+                                Toast.makeText(requireActivity(),"Coingecko",Toast.LENGTH_SHORT).show()
+                                val urlIntent = Intent(Intent.ACTION_VIEW, Uri.parse(coin_link))
+                                startActivity(urlIntent)
+                            }
+                        }
+                        if (website == "null"){ // Website
+                            binding.imTokenDetailSocialWebsite.visibility =View.GONE
+                        } else {
+                            binding.imTokenDetailSocialWebsite.visibility =View.VISIBLE
+                            binding.imTokenDetailSocialWebsite.setOnClickListener {
+                                Toast.makeText(requireActivity(),"Website",Toast.LENGTH_SHORT).show()
+                                val urlIntent = Intent(Intent.ACTION_VIEW, Uri.parse(website))
+                                startActivity(urlIntent)
+                            }
+                        }
+                    }
+
+                }
+                else if(jsonObject.get("response").equals("Failed")){
+                    Toast.makeText(requireActivity(), "Failed to Load Token Details", Toast.LENGTH_SHORT).show()
+                }
+                else
+                {
+                    Toast.makeText(requireActivity(),"Request error",Toast.LENGTH_SHORT).show()
+                }
+
+            }, Response.ErrorListener
+            {
+                    error ->
+                Toast.makeText(requireActivity(),error.toString(),Toast.LENGTH_SHORT).show()
+            })
+        {
+            override fun getParams(): HashMap<String, String>
+            {
+                val map = HashMap<String,String>()
+                map["request"] = "Sent"
+                map["name"] = "$tokenName"
+                return map
+            }
+        }
+
+        requestQueue.add(stringRequest)
+
+        stringRequest.retryPolicy = object : RetryPolicy {
+            override fun getCurrentTimeout(): Int {
+                return 20000
+            }
+
+            override fun getCurrentRetryCount(): Int {
+                return 20000
+            }
+
+            override fun retry(error: VolleyError?) {
+                return retry(error)
+            }
+        }
+    }
+
 }
