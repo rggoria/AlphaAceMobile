bb7fe55af73cb500c41c227c06aaeffa5a275fa5
Ram Emerson Goria <ramemersongoria.13.versus@gmail.com>
2022-10-07 09:55:43 +0800

    Add Database in Search Module

--- app/src/main/java/com/example/alphaacemobile/Constant.kt
+++ app/src/main/java/com/example/alphaacemobile/Constant.kt
@@ -4,7 +4,7 @@ object Constant {
     // Mac
     // const val BASE_URL = "http://192.168.1.108/AlphaAce/index.php/Connect/"
     // Ethernet adapter to Ethernet (use for all)
-    const val BASE_URL = "http://192.168.1.107/AlphaAce/index.php/Connect/"
+    const val BASE_URL = "http://192.168.1.109/AlphaAce/index.php/Connect/"
     // Ethernet adapter to VirtualBox Host-Only Network (For PC only)
     // const val BASE_URL = "http://192.168.56.1/AlphaAce/index.php/Connect/"
 }
--- app/src/main/java/com/example/alphaacemobile/fragment/SearchFragment.kt
+++ app/src/main/java/com/example/alphaacemobile/fragment/SearchFragment.kt
@@ -1,6 +1,7 @@
 package com.example.alphaacemobile.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,28 +10,19 @@ import android.widget.ArrayAdapter
 import android.widget.SearchView
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.alphaacemobile.Constant
 import com.example.alphaacemobile.R
 import com.example.alphaacemobile.databinding.FragmentSearchBinding
+import org.json.JSONObject
 
 class SearchFragment : Fragment() {
 
     private lateinit var binding: FragmentSearchBinding
     private lateinit var adapter: ArrayAdapter<String>
-
-    val tokenList =
-        listOf(
-            "ETH",
-            "ERC",
-            "RAM",
-            "PEJ",
-            "SHI",
-            "HI",
-            "HELLO",
-            "PRO",
-            "LOW",
-            "FLO"
-        )
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,13 +31,44 @@ class SearchFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentSearchBinding.inflate(inflater, container, false)
 
-        setupListView()
-        setupSearchView()
+        val url = Constant.BASE_URL + "get_top_token_home"
+        val requestQueue = Volley.newRequestQueue(requireActivity())
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener { response ->
+                val jsonObject = JSONObject(response)
+                if (jsonObject.get("response").equals("Success")){
+                    val jsonArray = jsonObject.getJSONArray("data")
+                    // Log.d("json", jsonArray.toString())
+
+                    for (i in 0..jsonArray.length()-1) {
+                        val jo = jsonArray.getJSONObject(i)
+                        //store your variable
+                        val token_name = jo.getString("token_name").toString()
+
+//                      Log.d("jarray", jsonArray.toString())
+                        Log.d("tk_name", token_name)
+                        val tokenList = listOf(token_name)
+//                        Toast.makeText(requireActivity(), "$tokenList",Toast.LENGTH_SHORT).show()
+                        Log.d("hi", token_name)
+                        setupListView(tokenList)
+                        setupSearchView(tokenList)
+                    }
+
+
+                }else{
+                    Toast.makeText(requireActivity(),jsonObject.get("response").toString(),Toast.LENGTH_SHORT).show()
+                }
+
+            }, Response.ErrorListener { error ->
+                Toast.makeText(requireActivity(),error.toString(), Toast.LENGTH_SHORT).show()
+            })
+        requestQueue.add(stringRequest)
 
         return binding.root
     }
 
-    private fun setupListView() {
+    private fun setupListView(tokenList: List<String>) {
         adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, tokenList)
         binding.lvSearch.adapter = adapter
         binding.lvSearch.setOnItemClickListener { parent, view, position, id ->
@@ -58,7 +81,7 @@ class SearchFragment : Fragment() {
         }
     }
 
-    private fun setupSearchView() {
+    private fun setupSearchView(tokenList: List<String>) {
         binding.svSearch.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
             androidx.appcompat.widget.SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
